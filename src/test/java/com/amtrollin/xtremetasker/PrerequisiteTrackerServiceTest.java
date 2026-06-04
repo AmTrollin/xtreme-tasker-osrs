4edef1544e948d142c158ef5c5d30de9a65be1ed
AmTrollin <dacodadavis@gmail.com>
2026-06-04 16:56:57 -0600

    Prepare v1 release build and fix Karamja diary prereqs

--- src/test/java/com/amtrollin/xtremetasker/PrerequisiteTrackerServiceTest.java
+++ src/test/java/com/amtrollin/xtremetasker/PrerequisiteTrackerServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.lang.reflect.Field;
 import java.lang.reflect.Proxy;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -27,6 +28,27 @@ public class PrerequisiteTrackerServiceTest
         assertTrue(complete.get(0).isCompleted());
     }
 
+    @Test
+    public void karamjaDiaryPrerequisiteUsesCompletionThreshold() throws Exception
+    {
+        PrerequisiteTrackerService partialService = serviceWithDiaryValue("DIARY_KARAMJA_EASY", 1001, 1);
+        List<PrerequisiteStatus> partial = partialService.evaluate("Complete the Karamja easy diary");
+        assertFalse("Karamja diary varbit increments by task and should not complete at 1/10",
+                partial.get(0).isCompleted());
+
+        PrerequisiteTrackerService completeService = serviceWithDiaryValue("DIARY_KARAMJA_EASY", 1001, 10);
+        List<PrerequisiteStatus> complete = completeService.evaluate("Complete the Karamja easy diary");
+        assertTrue(complete.get(0).isCompleted());
+    }
+
+    @Test
+    public void nonKaramjaDiaryPrerequisiteUsesPositiveVarbit() throws Exception
+    {
+        PrerequisiteTrackerService service = serviceWithDiaryValue("DIARY_ARDOUGNE_EASY", 1002, 1);
+        List<PrerequisiteStatus> statuses = service.evaluate("Complete the Ardougne easy diary");
+        assertTrue(statuses.get(0).isCompleted());
+    }
+
     private static PrerequisiteTrackerService serviceWithBrutFireValue(int brutFireValue) throws Exception
     {
         PrerequisiteTrackerService service = new PrerequisiteTrackerService();
@@ -36,7 +58,29 @@ public class PrerequisiteTrackerServiceTest
         return service;
     }
 
+    @SuppressWarnings("unchecked")
+    private static PrerequisiteTrackerService serviceWithDiaryValue(String varbitName, int varbitId, int varbitValue)
+            throws Exception
+    {
+        PrerequisiteTrackerService service = new PrerequisiteTrackerService();
+
+        Field varbitsField = PrerequisiteTrackerService.class.getDeclaredField("varbitsByName");
+        varbitsField.setAccessible(true);
+        Map<String, Integer> varbitsByName = (Map<String, Integer>) varbitsField.get(service);
+        varbitsByName.put(varbitName, varbitId);
+
+        Field clientField = PrerequisiteTrackerService.class.getDeclaredField("client");
+        clientField.setAccessible(true);
+        clientField.set(service, clientWithVarbitValue(varbitId, varbitValue));
+        return service;
+    }
+
     private static Client clientWithBrutFireValue(int brutFireValue)
+    {
+        return clientWithVarbitValue(VarbitID.BRUT_FIRE, brutFireValue);
+    }
+
+    private static Client clientWithVarbitValue(int varbitId, int varbitValue)
     {
         return (Client) Proxy.newProxyInstance(
                 Client.class.getClassLoader(),
@@ -47,9 +91,9 @@ public class PrerequisiteTrackerServiceTest
                             && args != null
                             && args.length == 1
                             && args[0] instanceof Integer
-                            && (Integer) args[0] == VarbitID.BRUT_FIRE)
+                            && (Integer) args[0] == varbitId)
                     {
-                        return brutFireValue;
+                        return varbitValue;
                     }
                     if ("getIntStack".equals(method.getName()))
                     {
